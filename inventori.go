c5b3305e535b1c36f86cd5216119ed98e2c58931
GerinAryoPrasetia <gerinaryo14@gmail.com>
2019-11-24 22:50:12 +0700

    tambah func

--- inventori.go
+++ inventori.go
@@ -16,10 +16,10 @@ type barang_jadi struct {
 	harga       int
 	asal_barang string
 }
-type array_mentah [idx_max]barang_mentah
-type array_jadi [idx_max]barang_jadi
 
 func main() {
+	var array_mentah [idx_max]barang_mentah
+	var array_jadi [idx_max]barang_jadi
 	var input_user int
 	array_mentah[1].nama = "bawang"
 	array_jadi[1].nama = "nasi goreng"
@@ -29,34 +29,35 @@ func main() {
 	}
 }
 
-func menu(){
+func menu() {
+	var input_user int
 	fmt.Println("1. Tampilkan Barang")
-		fmt.Println("2. Cari Barang")
-		fmt.Println("3. Edit Barang")
-		fmt.Println("4. Hapus Barang")
-		if input_user == 1 {
-			tampil_barang()
-		} else if input_user == 2 {
-			cari_barang()
-		} else if input_user == 3 {
-			edit_barang()
-		} else if input_user == 4 {
-			hapus_barang()
-		}
-}
-
-func tampil_barang(tab *array_mentah) {
-	var i int
-	for i <= idx_max {
-		fmt.Println(i, ". ", *tab[i])
+	fmt.Println("2. Cari Barang")
+	fmt.Println("3. Edit Barang")
+	fmt.Println("4. Hapus Barang")
+	if input_user == 1 {
+		tampil_barang(&barang_jadi)
 	}
 }
 
-func sorting_mentah(tab *array_mentah) {
+func tampil_barang(tab *barang_mentah) {
+	var i int
+	var input_user int
+	for i <= idx_max {
+		fmt.Println(i, ". ", *tab[i])
+		if input_user == 1 {
+			sorting_mentah(&barang_mentah)
+		} else if input_user == 2 {
+			sorting_jadi(&barang_jadi)
+		}
+	}
+}
+
+func sorting_mentah(tab *barang_mentah) {
 	for i := 0; i < len(*tab); i++ {
 		min_index := 1
-		for j := i + 1; j<len(*tab); j++{
-			if (*tab)[min_index] > (*tab)array_mentah[j]{
+		for j := i + 1; j < len(*tab); j++ {
+			if (*tab)[min_index] > (*tab)[j] {
 				min_index = j
 			}
 		}
@@ -66,12 +67,13 @@ func sorting_mentah(tab *array_mentah) {
 	}
 }
 
-func sorting_jadi(tab *array_mentah){
+func sorting_jadi(tab *array_mentah) {
 	var min_index int
-	for i := 0; i < len(*tab){
+	var j int
+	for i := 0; i < len(*tab); i++ {
 		min_index = 1
-		for j := i + 1; j < len(*tab); j++{
-			if (*tab)[min_index] > array_mentah[j]{
+		for j := i + 1; j < len(*tab); j++ {
+			if (*tab)[min_index] > array_mentah[j] {
 				min_index = j
 			}
 		}
